63ac068da1886e5f0d37f67003fa1d72037a618b
Luis Espinosa <lluisllanoss@gmail.com>
2022-05-29 00:36:01 -0500

    Command handler and mediator

---
+++ account.cmd/src/main/java/com/techbank/account/cmd/infrastructure/AccountEventStore.java
@@ -0,0 +1,58 @@
+package com.techbank.account.cmd.infrastructure;
+
+import com.techbank.account.cmd.domain.AccountAggregate;
+import com.techbank.account.cmd.domain.EventStoreRepository;
+import com.techbank.cqrs.core.events.BaseEvent;
+import com.techbank.cqrs.core.events.EventModel;
+import com.techbank.cqrs.core.exceptions.AggregateNotFoundException;
+import com.techbank.cqrs.core.exceptions.ConcurrencyException;
+import com.techbank.cqrs.core.infrastructure.EventStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class AccountEventStore implements EventStore {
+
+    @Autowired
+    private EventStoreRepository eventStoreRepository;
+
+    @Override
+    public void saveEvents(String aggregateId, Iterable<BaseEvent> events, int expectedVersion) {
+        var eventStream = eventStoreRepository.findByAggregateIdentifier(aggregateId);
+        if (expectedVersion != -1 && eventStream.get(eventStream.size() - 1).getVersion() != expectedVersion) {
+            throw new ConcurrencyException();
+        }
+        var version = expectedVersion;
+        EventModel eventModel = null;
+        for (var event : events) {
+            version++;
+            event.setVersion(version);
+            eventModel = EventModel.builder()
+                    .timestamp(new Date())
+                    .aggregateIdentifier(aggregateId)
+                    .aggregateType(AccountAggregate.class.getTypeName())
+                    .version(version)
+                    .eventType(event.getClass().getTypeName())
+                    .eventData(event)
+                    .build();
+
+            var persistedEvent = eventStoreRepository.save(eventModel);
+            if (persistedEvent != null) {
+                //TODO: produce event to kafka
+            }
+        }
+    }
+
+    @Override
+    public List<BaseEvent> getEvents(String aggregateId) {
+        var eventStream = eventStoreRepository.findByAggregateIdentifier(aggregateId);
+        if (eventStream == null || eventStream.isEmpty()){
+            throw new AggregateNotFoundException("Incorrec account ID provided");
+        }
+        return eventStream.stream().map(x -> x.getEventData()).collect(Collectors.toList());
+    }
+}
